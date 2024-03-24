af1aa3e87d45f096b3a9c8cb3952fe7e61f9fb00
Fcomeon <2221035372@qq.com>
2024-03-24 15:17:21 +0800

    C语言程序设计第一章习题实践

---
+++ Study Test/FirstChapter_Questioins.c
@@ -0,0 +1,50 @@
+# define _CRT_SECURE_NO_WARNINGS 1
+# include <stdio.h>
+# include <math.h>
+
+
+//
+//void main()
+//{
+//	printf("Hello World!");
+//}
+
+
+
+//int main()
+//{
+//	int x, y;
+//
+//	for (x = 1; x < 5; x++)
+//	{
+//		switch (x)
+//		{
+//		case 1:
+//			printf("\n");
+//			printf("");
+//			break;
+//		case 2:
+//			printf("\n");
+//			printf("  ");
+//			break;
+//		case 3:
+//			printf("\n");
+//			printf("    ");
+//			break;
+//		case 4:
+//			printf("\n");
+//			printf("      ");
+//			break;
+//		}
+//
+//		for (y = 0; y < 5; y++)
+//		{
+//			printf("*");
+//		}
+//
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
